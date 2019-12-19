9fe709f50be660a7f41d9b0b2fd001cff4735fa1
Varlese <mvarlese@deloitte.it>
2019-12-19 15:58:47 +0100

    New version of springboot app patch

--- dbTestEsempio2/target/dependency/BOOT-INF/classes/static/hello.js
+++ dbTestEsempio2/target/dependency/BOOT-INF/classes/static/hello.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://localhost:8080/demo/all"
+        url: "http://localhost:8080/demo/all",
+        jsonp: "$jsonp",
+        dataType: "jsonp
     }).then(function(data) {
     	console.log(data);
     	for(var i = 0; i < data.length; i++) {
